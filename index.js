caa04999f6e2cacee472a10116a5acbd18eec49b
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-01-23 19:21:43 +0900

    Change the order of function (don't add code)

--- index.js
+++ index.js
@@ -9,51 +9,10 @@ const SEEK_DIRECTION_STOP ="S";     //移動しない
 //次行の順番で、余分な空白の含まない
 // [ 状態 , テープヘッドの値 , 遷移先の状態 , 書き換える値 , ヘッドの移動先 ]
 
-
-function startCalc(){   //startを押したときに実行される
-    console.log("start");
-    const CSVtext = document.getElementById("StateTransitionText").value;
-    const firstTape = document.getElementById("tapeText").value;
-    const stateTransition = csv2array(CSVtext);
-    turingMachineSimuration(firstTape,stateTransition);
-
-    function csv2array(csvData){    //csv形式の文字列を二次元配列に変換
-        const lines = csvData.split("\n");
-        const csvArray = new Array();
-        lines.forEach((line,idx)=>{
-            csvArray[idx]=line.split(",");
-        });
-        return csvArray;
-    }
-}
-
-function seek(directon){ // ヘッドの移動
-    switch(directon){
-        case SEEK_DIRECTION_LEFT:
-            return -1;
-        case SEEK_DIRECTION_RIGHT:
-            return +1;
-        case SEEK_DIRECTION_STOP:
-            return 0;
-    }
-}
-
-function stateTransition(stateTransitionTable,config){ // 状態遷移
-    //        0    1         2       3                 4
-    // delta=[状態,ヘッドの値,遷移状態,ヘッドの書き換え値,シーク方向]
-    for(const delta of stateTransitionTable){
-        if(delta[0] == config.state && delta[1]==config.tape[config.head]){
-            config.tape = config.tape.slice(0,config.head) + delta[3] + config.tape.slice(Number(config.head)+1)
-            config.head += seek(delta[4]);
-            config.state = delta[2];
-//            return config;
-            return;
-        }
-    }
-    //ここでチューリングマシンはストップするはず。
-    //現在の実装では、様相が全く変化しないことでループ検知が働いている。
-//    return config;
-    return;  
+function configuration(tape,head,state){ //様相のオブジェクト(構造体)
+    this.tape = tape;
+    this.head = head;
+    this.state = state;
 }
 
 function turingMachineSimuration(tapeOrigin,stateTransitionTable){  // シュミレータ本体
@@ -96,6 +55,35 @@ function turingMachineSimuration(tapeOrigin,stateTransitionTable){  // シュミ
     }
 }
 
+function seek(directon){ // ヘッドの移動
+    switch(directon){
+        case SEEK_DIRECTION_LEFT:
+            return -1;
+        case SEEK_DIRECTION_RIGHT:
+            return +1;
+        case SEEK_DIRECTION_STOP:
+            return 0;
+    }
+}
+
+function stateTransition(stateTransitionTable,config){ // 状態遷移
+    //        0    1         2       3                 4
+    // delta=[状態,ヘッドの値,遷移状態,ヘッドの書き換え値,シーク方向]
+    for(const delta of stateTransitionTable){
+        if(delta[0] == config.state && delta[1]==config.tape[config.head]){
+            config.tape = config.tape.slice(0,config.head) + delta[3] + config.tape.slice(Number(config.head)+1)
+            config.head += seek(delta[4]);
+            config.state = delta[2];
+//            return config;
+            return;
+        }
+    }
+    //ここでチューリングマシンはストップするはず。
+    //現在の実装では、様相が全く変化しないことでループ検知が働いている。
+//    return config;
+    return;  
+}
+
 function printTapeAndState(config){ //様相を表示
     console.log(config.tape +"   [status:"+config.state+"]");
     let text="";
@@ -103,9 +91,19 @@ function printTapeAndState(config){ //様相を表示
     console.log(text+"^");
 }
 
-function configuration(tape,head,state){ //様相のオブジェクト(構造体)
-    this.tape = tape;
-    this.head = head;
-    this.state = state;
-}
+function startCalc(){   //startを押したときに実行される
+    console.log("start");
+    const CSVtext = document.getElementById("StateTransitionText").value;
+    const firstTape = document.getElementById("tapeText").value;
+    const stateTransition = csv2array(CSVtext);
+    turingMachineSimuration(firstTape,stateTransition);
 
+    function csv2array(csvData){    //csv形式の文字列を二次元配列に変換
+        const lines = csvData.split("\n");
+        const csvArray = new Array();
+        lines.forEach((line,idx)=>{
+            csvArray[idx]=line.split(",");
+        });
+        return csvArray;
+    }
+}
