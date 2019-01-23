44895c7126550f9562a41e4c29ba0abe71f89181
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-01-23 20:26:13 +0900

    if click button, play a step of turing machine

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const SEEK_DIRECTION_STOP ="S";     //移動しない
 //csvの形式
 //次行の順番で、余分な空白の含まない
 // [ 状態 , テープヘッドの値 , 遷移先の状態 , 書き換える値 , ヘッドの移動先 ]
+const config = new configuration("","","");
+const tapeLog= new Array();
+let stateTransitionTable= new Array();
+
+let playingTuringMachine = false;
 
 function configuration(tape,head,state){ //様相のオブジェクト(構造体)
     this.tape = tape;
@@ -15,44 +20,55 @@ function configuration(tape,head,state){ //様相のオブジェクト(構造体
     this.state = state;
 }
 
-function turingMachineSimuration(tapeOrigin,stateTransitionTable){  // シュミレータ本体
-    let config = new configuration(`_${tapeOrigin}_`,HEAD_INITIAL_POSITION,STATE_INITIAL);
-
-    const tapeLog= new Array();
+function loadTapeAndTransitionFromHTML(){
+    const CSVtext = document.getElementById("StateTransitionText").value;
+    const tapeOrigin = document.getElementById("tapeText").value;
+    const transition = csv2array(CSVtext);
+    initializeTuringMachine(tapeOrigin,transition);
+    printTapeAndState(config);
     tapeLog.push(Object.assign({},config));
+}
 
+function initializeTuringMachine(tapeOrigin,transition){
+    config.tape=`_${tapeOrigin}_`;
+    config.head=HEAD_INITIAL_POSITION;
+    config.state=STATE_INITIAL;
+    stateTransitionTable = transition;
+    playingTuringMachine = true;
+}
+
+function nextStep(){
+    if(playingTuringMachine==false)
+        return;
+
+    stateTransition(stateTransitionTable,config);
     printTapeAndState(config);
 
-    while(!(config.state===STATE_ACCEPT) && !(config.state===STATE_REJECT)){
-        stateTransition(stateTransitionTable,config);
-
-        printTapeAndState(config);
-
-        //受理されない場合
-            // LOOP テープ、ヘッド、状態が過去の少なくともある一時点と一致したら、ループなので受理されない。
-            // 遷移先が指定されずに停止した場合もここに含まれる。
-        const matchConfig = tapeLog.filter((pastConfig)=>{
-            return pastConfig.tape == config.tape 
-              &&pastConfig.head == config.head
-              &&pastConfig.state == config.state
-        });
-        if(matchConfig.length>0){
-            console.log("(!!WILL NOT STOP!!) It is looped. ");
-            return 0;
-        }
-        tapeLog.push(Object.assign({},config));
-        // 拒否状態
-        if(config.state===STATE_REJECT){
-            console.log("(!!REJECT!!) It is rejected. ");
-            return 0;
-        }
-
-        //受理される場合   
-        if(config.state===STATE_ACCEPT){
-            console.log("(ACCEPT) It is accepted. ");
-            return 0;
-        }
+    //受理されない場合
+        // LOOP テープ、ヘッド、状態が過去の少なくともある一時点と一致したら、ループなので受理されない。
+        // 遷移先が指定されずに停止した場合もここに含まれる。
+    const matchConfig = tapeLog.filter((pastConfig)=>{
+        return pastConfig.tape == config.tape 
+          &&pastConfig.head == config.head
+          &&pastConfig.state == config.state
+    });
+    if(matchConfig.length>0){
+        console.log("(!!WILL NOT STOP!!) It is looped. ");
+        playingTuringMachine = false;
     }
+    tapeLog.push(Object.assign({},config));
+    // 拒否状態
+    if(config.state===STATE_REJECT){
+        console.log("(!!REJECT!!) It is rejected. ");
+        playingTuringMachine = false;
+    }
+
+    //受理される場合   
+    if(config.state===STATE_ACCEPT){
+        console.log("(ACCEPT) It is accepted. ");
+        playingTuringMachine = false;
+    }
+    
 }
 
 function seek(directon){ // ヘッドの移動
@@ -74,13 +90,11 @@ function stateTransition(stateTransitionTable,config){ // 状態遷移
             config.tape = config.tape.slice(0,config.head) + delta[3] + config.tape.slice(Number(config.head)+1)
             config.head += seek(delta[4]);
             config.state = delta[2];
-//            return config;
             return;
         }
     }
-    //ここでチューリングマシンはストップするはず。
-    //現在の実装では、様相が全く変化しないことでループ検知が働いている。
-//    return config;
+    console.log("(!!STOP!!) It is stopped. ");
+    playingTuringMachine = false;
     return;  
 }
 
@@ -91,19 +105,11 @@ function printTapeAndState(config){ //様相を表示
     console.log(text+"^");
 }
 
-function startCalc(){   //startを押したときに実行される
-    console.log("start");
-    const CSVtext = document.getElementById("StateTransitionText").value;
-    const firstTape = document.getElementById("tapeText").value;
-    const stateTransition = csv2array(CSVtext);
-    turingMachineSimuration(firstTape,stateTransition);
-
-    function csv2array(csvData){    //csv形式の文字列を二次元配列に変換
-        const lines = csvData.split("\n");
-        const csvArray = new Array();
-        lines.forEach((line,idx)=>{
-            csvArray[idx]=line.split(",");
-        });
-        return csvArray;
-    }
+function csv2array(csvData){    //csv形式の文字列を二次元配列に変換
+    const lines = csvData.split("\n");
+    const csvArray = new Array();
+    lines.forEach((line,idx)=>{
+        csvArray[idx]=line.split(",");
+    });
+    return csvArray;
 }
